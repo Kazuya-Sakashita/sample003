b14b40192ffd43b3c0139097b2d9baa09c4122f3
Kazuya-Sakashita <kz0508@gmail.com>
2022-01-08 07:17:42 +0900

    env REDIS_URL修正

--- app/mailers/contact_monthly_mailer.rb
+++ app/mailers/contact_monthly_mailer.rb
@@ -3,7 +3,7 @@ class ContactMonthlyMailer < ApplicationMailer
           from: 'notification@example.com'
 
   def contact_monthly_mail(user)
-    @user = user
+    @user = params[:user]
     mail(subject: "稼働時間入力のお願い")
   end
 end
