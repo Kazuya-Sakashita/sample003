80b82a48da51e45269c6a8e36e92c1f765719fa4
Kazuya-Sakashita <kz0508@gmail.com>
2022-01-08 15:58:15 +0900

    test

--- app/mailers/contact_monthly_mailer.rb
+++ app/mailers/contact_monthly_mailer.rb
@@ -2,8 +2,8 @@ class ContactMonthlyMailer < ApplicationMailer
   default bcc: -> { User.pluck(:email) },
           from: 'notification@example.com'
 
-  def contact_monthly_mail(user)
-    @user = params[:user]
+  def contact_monthly_mail(current_user)
+    @user = current_user
     mail(subject: "稼働時間入力のお願い")
   end
 end
