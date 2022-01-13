43ad1757e8a12a7a69b91c72c0b9421b08275b87
Kazuya-Sakashita <kz0508@gmail.com>
2022-01-14 07:52:36 +0900

    css

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import '../css/tailwind.css';
+import "stylesheets/application.css"
 
 Rails.start()
 Turbolinks.start()
